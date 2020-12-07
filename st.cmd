3f25494face9db2131fb316e4efdfd38712074bc
Maksim Rakitin <mrakitin@bnl.gov>
2020-12-07 13:07:06 -0500

    Sync st.cmd file

--- st.cmd
+++ st.cmd
@@ -16,6 +16,14 @@ jupyter_log="$log_dir/jupyter-fmx_$(date "+%F_%H-%M-%S").log"
 . /opt/conda/etc/profile.d/conda.sh
 
 # source activate /opt/conda_envs/collection-17Q3.0
-conda activate /opt/conda_envs/collection-2019-2.0-fmx
+# conda activate /opt/conda_envs/collection-2019-2.0-fmx
+# conda activate /opt/conda_envs/collection-2019-3.0-fmx
+conda activate /opt/conda_envs/collection-2020-2.0rc8
+
+###############################################################################
+# DAMA note (July 10, 2019): the corresponding kernel file is defined in      #
+# /usr/share/jupyter/kernels-available/collection-2019-3.0/kernel.json, and   #
+# symlinked to /usr/share/jupyter/kernels/collection-2019-3.0/kernel.json     #
+###############################################################################
 
 jupyter lab --no-browser --notebook-dir=/epics/iocs/notebook/notebooks/ --ip=0.0.0.0 --port=17000 --NotebookApp.token='' --debug > $jupyter_log 2>&1
